acd129dabe9ae7ead031a3768b301ed321d4f014
Shantaram Kokate <shantaramkokate@MyWayIT.local>
2018-08-16 13:40:20 +0530

    first commit

---
+++ FacebookLogin/AppDelegate.swift
@@ -0,0 +1,51 @@
+//
+//  AppDelegate.swift
+//  FacebookLogin
+//
+//  Created by Shantaram Kokate on 8/16/18.
+//  Copyright © 2018 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    //added these 3 methods
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FBSDKLoginManager.initialize()  // Initialize facebook login
+         return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func applicationWillResignActive(_ application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return FBSDKApplicationDelegate.sharedInstance().application(application, open: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
+
+ 
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ FacebookLogin/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  FacebookLogin
+//
+//  Created by Shantaram Kokate on 8/16/18.
+//  Copyright © 2018 Shantaram Kokate. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FacebookLogin
+class ViewController: UIViewController,FBSDKLoginButtonDelegate {
+
+    var dict : [String : AnyObject]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //creating button
+        let loginButton = FBSDKLoginButton(type: .roundedRect)
+        let parameters = ["public_profile","email"]
+        loginButton.readPermissions = parameters
+        loginButton.center = view.center
+        loginButton.delegate = self
+       // loginButton.addTarget(self, action: #selector(loginButtonClicked), for: .touchDragInside)
+        //adding it to view
+        view.addSubview(loginButton)
+        //if the user is already logged in
+        if (FBSDKAccessToken.current()) != nil{
+            getFBUserData()
+        }
+        
+    }
+    
+    //when login button clicked
+    @objc func loginButtonClicked() {
+        let loginManager = FBSDKLoginManager()
+        loginManager.logIn(withReadPermissions: ["public_profile","email"], from: self) { (loginResult, error) in
+            if (error == nil) {
+                let fbloginresult : FBSDKLoginManagerLoginResult = loginResult!
+                if fbloginresult.grantedPermissions != nil {
+                    if(fbloginresult.grantedPermissions.contains("email"))
+                    {
+                        self.getFBUserData()
+                        loginManager.logOut()
+                    }
+                    
+                }
+                
+            }
+        }
+    }
+    
+    //function is fetching the user data
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            //    let parameters = ["fields": "first_name, email, last_name, picture"]
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    // MARK: - Facebook login delegate
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if (error == nil) {
+            if (FBSDKAccessToken.current()) != nil{
+                getFBUserData()
+                FBSDKLoginManager().logOut()
+            }
+        }
+    }
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+
+}
+
